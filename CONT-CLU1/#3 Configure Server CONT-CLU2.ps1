c44b3f6844bd50f7d824a58fbf9ac9097180c27c
christianhjohnson <89424695+christianhjohnson@users.noreply.github.com>
2021-10-24 14:00:25 -0700

    Updated CLU Config

--- CONT-CLU1/#3 Configure Server CONT-CLU2.ps1
+++ CONT-CLU1/#3 Configure Server CONT-CLU2.ps1
@@ -1,8 +1,8 @@
 #Enter PowerShell Session
-Enter-PSSession -VMName "CONT-CLU1"
+Enter-PSSession -VMName "CONT-CLU2"
 
 #Configure new Server
-Rename-Computer -NewName "CONT-CLU1"
+Rename-Computer -NewName "CONT-CLU2"
 
 #Activate Remote Desktop
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0
@@ -11,7 +11,7 @@ Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
 
 #Set IP Address
-New-NetIPAddress -InterfaceIndex 2 -IPAddress 192.168.1.160 -PrefixLength 24
+New-NetIPAddress -InterfaceIndex 2 -IPAddress 192.168.1.161 -PrefixLength 24
 
 #Set DNS Client Address
 Set-DnsClientServerAddress -InterfaceIndex 2 -ServerAddresses 192.168.1.152
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # The Contoso
 Building a windows server environment and connecting with Azure.
 
+Issues so far:
+When creating reference images with MDT always mount the ISO's as a DVD on the local computer.  DO NOT try to use the boot USB drive.
+
+
+
