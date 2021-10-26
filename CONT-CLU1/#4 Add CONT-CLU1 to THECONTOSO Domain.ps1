f7d775e9e291b7d70b1c1793d601ddacf606c81e
christianhjohnson <89424695+christianhjohnson@users.noreply.github.com>
2021-10-26 10:44:15 -0700

    Changed Add-Computer to include both strings to get credentials

--- CONT-CLU1/#4 Add CONT-CLU1 to THECONTOSO Domain.ps1
+++ CONT-CLU1/#4 Add CONT-CLU1 to THECONTOSO Domain.ps1
@@ -1,5 +1,6 @@
 $cred = Get-Credential thecontoso.com\administrator
+$local = Get-Credential CONT-CLU1\Administrator
 New-PSSession -VMName "CONT-CLU1" -Credential $cred
-Add-Computer -DomainName THECONTOSO.COM
+Add-Computer -ComputerName CONT-CLU1 -LocalCredential $local -DomainName Thecontoso.com -Credential $cred
 Restart-Computer
 Remove-PSSession
--- CONT-CLU2/#4 Add CONT-CLU2 to THECONTOSO Domain.ps1
+++ CONT-CLU2/#4 Add CONT-CLU2 to THECONTOSO Domain.ps1
@@ -1,5 +1,6 @@
 $cred = Get-Credential thecontoso.com\administrator
+$local = Get-Credential CONT-CLU2\Administrator
 New-PSSession -VMName "CONT-CLU2" -Credential $cred
-Add-Computer -DomainName THECONTOSO.COM
+Add-Computer -ComputerName CONT-CLU2 -LocalCredential $local -DomainName Thecontoso.com -Credential $cred
 Restart-Computer
 Remove-PSSession
