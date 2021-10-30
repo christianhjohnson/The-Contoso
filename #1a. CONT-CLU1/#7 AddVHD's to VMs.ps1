97cbeb4f5b254f9bbc45192acf116ab4968a8b36
christianhjohnson <89424695+christianhjohnson@users.noreply.github.com>
2021-10-30 15:08:53 -0700

    Small fixes to scripts

--- #1a. CONT-CLU1/#7 AddVHD's to VMs.ps1
+++ #1a. CONT-CLU1/#7 AddVHD's to VMs.ps1
@@ -1,6 +1,6 @@
 $VMName = "CONT-CLU1"
-Add-VMHardDiskDrive -VMName $VMName -Path 'E:\VMs\CONT-CLU1\ Wintessdisk.vhdx'
+Add-VMHardDiskDrive -VMName $VMName -Path 'E:\VMs\CONT-CLU1\Wintessdisk.vhdx'
 for ($DiskNumber = 2; $DiskNumber -lt 5; $DiskNumber++)
 {
-Add-VMHardDiskDrive -VMName $VMName -Path "E:\VMs\CONT-CLU1\ Datadisk$Disknumber.vhdx"
+Add-VMHardDiskDrive -VMName $VMName -Path "E:\VMs\CONT-CLU1\Datadisk$Disknumber.vhdx"
 }
