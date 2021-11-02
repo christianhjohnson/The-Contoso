9bf6af345a860b2b0995192de10da84889893796
christianhjohnson <89424695+christianhjohnson@users.noreply.github.com>
2021-11-02 10:27:54 -0700

    Fixed get-credential placing

--- #1a. CONT-CLU1/#5CONT-CLU1.ps1
+++ #1a. CONT-CLU1/#5CONT-CLU1.ps1
@@ -1,5 +1,5 @@
-$creddc = Get-Credential thecontoso.com\administrator
 $local = Get-Credential Administrator
 New-PSSession -VMName "CONT-CLU1" -Credential $local
+$creddc = Get-Credential thecontoso.com\administrator
 Add-Computer -DomainName Thecontoso.com -Credential $creddc
 Restart-Computer -Force
