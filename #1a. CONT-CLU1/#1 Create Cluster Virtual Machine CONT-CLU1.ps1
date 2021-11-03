56b74dde7bf73b3bed95d36e28fcf1d53095ee3d
christianhjohnson <89424695+christianhjohnson@users.noreply.github.com>
2021-11-03 15:15:40 -0700

    Consolidated Create VM + Create Disks + Add VHDX to VMs

--- #1a. CONT-CLU1/#1 Create Cluster Virtual Machine CONT-CLU1.ps1
+++ #1a. CONT-CLU1/#1 Create Cluster Virtual Machine CONT-CLU1.ps1
@@ -9,5 +9,8 @@ for ($VMName = 1; $VMName -lt 3; $VMName++)
     New-VHD -Path C:\WitnessDisk\CLU$VMName\Witnessdisk.vhdx -Fixed SizeBytes 1GB
     New-VHD -Path F:\ClusterDisk\CLU$VMName\Clusterdisk1.vhdx -Dynamic SizeBytes 40GB
     New-VHD -Path H:\ClusterDisk\CLU$VMName\Clusterdisk2.vhdx -Dynamic SizeBytes 40GB
+    Add-VMHardDiskDrive -VMName CONT-CLU$VMName -Path C:\WitnessDisk\CLU$VMName\Witnessdisk.vhdx
+    Add-VMHardDiskDrive -VMName CONT-CLU$VMName -Path F:\ClusterDisk\CLU$VMName\Clusterdisk1.vhdx
+    Add-VMHardDiskDrive -VMName CONT-CLU$VMName -Path H:\ClusterDisk\CLU$VMName\Clusterdisk2.vhdx
 }
 
--- #2. CONT-CLU1&2/#1 Create Witness and Datadisks for Failov.ps1
+++ #2. CONT-CLU1&2/#1 Create Witness and Datadisks for Failov.ps1
@@ -1,7 +1,9 @@
-#Create Witness and Datadisks for Failover Cluster
+<#Create Witness and Datadisks for Failover Cluster
+Spanning multiple physical disks on server
+#>
 New-VHD -Path C:\WitnessDisk\CONT-CLU1\Witnessdisk.vhdx -Fixed SIzeBytes 1GB
 New-VHD -Path F:\ClusterDisk\CONT-CLU1\Clusterdisk1.vhdx -Dynamic SIzeBytes 40GB
-New-VHD -Path C:\ClusterDisk\CONT-CLU1\Clusterdisk2.vhdx -Dynamic SIzeBytes 40GB
+New-VHD -Path H:\ClusterDisk\CONT-CLU1\Clusterdisk2.vhdx -Dynamic SIzeBytes 40GB
 New-VHD -Path C:\WitnessDisk\CONT-CLU2\Witnessdisk.vhdx -Fixed SIzeBytes 1GB
 New-VHD -Path F:\ClusterDisk\CONT-CLU2\Clusterdisk1.vhdx -Dynamic SIzeBytes 40GB
-New-VHD -Path C:\ClusterDisk\CONT-CLU2\Clusterdisk2.vhdx -Dynamic SIzeBytes 40GB
+New-VHD -Path H:\ClusterDisk\CONT-CLU2\Clusterdisk2.vhdx -Dynamic SIzeBytes 40GB
