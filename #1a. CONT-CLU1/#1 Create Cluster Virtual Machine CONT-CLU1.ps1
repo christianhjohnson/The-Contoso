b4718b3a67b87795c5604554fe4ef04f2b3a9a52
christianhjohnson <89424695+christianhjohnson@users.noreply.github.com>
2021-11-03 14:46:59 -0700

    Update #1 Create Cluster Virtual Machine CONT-CLU1.ps1

--- #1a. CONT-CLU1/#1 Create Cluster Virtual Machine CONT-CLU1.ps1
+++ #1a. CONT-CLU1/#1 Create Cluster Virtual Machine CONT-CLU1.ps1
@@ -1,6 +1,13 @@
-$VMNames = "CONT-CLU1","CONT-CLU2"
-New-VM -Name $VMNames -MemoryStartupBytes 4GB -Generation 2 -NewVHDPath E:\VMs\$VMNames.vhdx -NewVHDSizeBytes 40GB -Switch Extswitch1
-Add-VMDVDDrive -VMName $VMNames -Path C:\ISOs\WindowsServer2016.iso
-$DVDDrive = Get-VMDvdDrive $VMNames
-$primaryDrive = Get-VMHardDiskDrive -VMName $VMNames
-Set-VMFirmware $VMNames -BootOrder $primaryDrive, $DVDDrive
+#Create Failover Cluster Nodes with Data Disks
+for ($VMName = 1; $VMName -lt 3; $VMName++)
+{
+    New-VM -Name CONT-CLU$VMName -MemoryStartupBytes 4GB -Generation 2 -NewVHDPath E:\VMs\CONT-CLU$VMName.vhdx -NewVHDSizeBytes 40GB -Switch InternalSwitch
+    Add-VMDVDDrive -VMName CONT-CLU$VMName -Path C:\ISOs\WindowsServer2016.iso
+    $DVDDrive = Get-VMDvdDrive CONT-CLU$VMName
+    $primaryDrive = Get-VMHardDiskDrive -VMName $VMName
+    Set-VMFirmware CONT-CLU$VMName -BootOrder $primaryDrive, $DVDDrive
+    New-VHD -Path C:\WitnessDisk\CLU$VMName\Witnessdisk.vhdx -Fixed SizeBytes 1GB
+    New-VHD -Path F:\ClusterDisk\CLU$VMName\Clusterdisk1.vhdx -Dynamic SizeBytes 40GB
+    New-VHD -Path H:\ClusterDisk\CLU$VMName\Clusterdisk2.vhdx -Dynamic SizeBytes 40GB
+}
+
