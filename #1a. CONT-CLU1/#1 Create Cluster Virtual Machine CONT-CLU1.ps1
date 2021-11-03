e833c5db2b65d56cc31df6b243180b91b76e93c7
christianhjohnson <89424695+christianhjohnson@users.noreply.github.com>
2021-11-03 15:54:54 -0700

    It WORKS!!!!!

--- #1a. CONT-CLU1/#1 Create Cluster Virtual Machine CONT-CLU1.ps1
+++ #1a. CONT-CLU1/#1 Create Cluster Virtual Machine CONT-CLU1.ps1
@@ -1,16 +1,16 @@
-#Create Failover Cluster Nodes with Data Disks
-for ($VMName = 1; $VMName -lt 3; $VMName++)
+#Create Failover Cluster Nodes with Data Disks + add the disks to each VM
+for ($VMNumber = 1; $VMNumber -lt 3; $VMNumber++)
 {
-    New-VM -Name CONT-CLU$VMName -MemoryStartupBytes 4GB -Generation 2 -NewVHDPath E:\VMs\CONT-CLU$VMName.vhdx -NewVHDSizeBytes 40GB -Switch InternalSwitch
-    Add-VMDVDDrive -VMName CONT-CLU$VMName -Path C:\ISOs\WindowsServer2016.iso
-    $DVDDrive = Get-VMDvdDrive CONT-CLU$VMName
-    $primaryDrive = Get-VMHardDiskDrive -VMName $VMName
-    Set-VMFirmware CONT-CLU$VMName -BootOrder $primaryDrive, $DVDDrive
-    New-VHD -Path C:\WitnessDisk\CLU$VMName\Witnessdisk.vhdx -Fixed SizeBytes 1GB
-    New-VHD -Path F:\ClusterDisk\CLU$VMName\Clusterdisk1.vhdx -Dynamic SizeBytes 40GB
-    New-VHD -Path H:\ClusterDisk\CLU$VMName\Clusterdisk2.vhdx -Dynamic SizeBytes 40GB
-    Add-VMHardDiskDrive -VMName CONT-CLU$VMName -Path C:\WitnessDisk\CLU$VMName\Witnessdisk.vhdx
-    Add-VMHardDiskDrive -VMName CONT-CLU$VMName -Path F:\ClusterDisk\CLU$VMName\Clusterdisk1.vhdx
-    Add-VMHardDiskDrive -VMName CONT-CLU$VMName -Path H:\ClusterDisk\CLU$VMName\Clusterdisk2.vhdx
+    New-VM -Name CONT-CLU$VMNumber -MemoryStartupBytes 4GB -Generation 2 -NewVHDPath E:\VMs\CONT-CLU$VMNumber.vhdx -NewVHDSizeBytes 40GB -Switch InternalSwitch
+    Add-VMDVDDrive -VMName CONT-CLU$VMNumber -Path C:\ISOs\WindowsServer2016.iso
+    $DVDDrive = Get-VMDvdDrive CONT-CLU$VMNumber
+    $primaryDrive = Get-VMHardDiskDrive -VMName CONT-CLU$VMNumber
+    Set-VMFirmware CONT-CLU$VMNumber -BootOrder $primaryDrive, $DVDDrive
+    New-VHD -Path "C:\WitnessDisk\CLU$VMNumber\Witnessdisk.vhdx" -Fixed -SizeBytes 1GB
+    New-VHD -Path "F:\ClusterDisk\CLU$VMNumber\Clusterdisk1.vhdx" -Dynamic -SizeBytes 40GB 
+    New-VHD -Path "H:\ClusterDisk\CLU$VMNumber\Clusterdisk2.vhdx" -Dynamic -SizeBytes 40GB 
+    Add-VMHardDiskDrive -VMName CONT-CLU$VMNumber -Path "C:\WitnessDisk\CLU$VMNumber\Witnessdisk.vhdx"
+    Add-VMHardDiskDrive -VMName CONT-CLU$VMNumber -Path "F:\ClusterDisk\CLU$VMNumber\Clusterdisk1.vhdx"
+    Add-VMHardDiskDrive -VMName CONT-CLU$VMNumber -Path "H:\ClusterDisk\CLU$VMNumber\Clusterdisk2.vhdx"
 }
 
