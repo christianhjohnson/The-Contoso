9d47946e2aff27b3379b7c756ab2a65a578ee0f4
christianhjohnson <89424695+christianhjohnson@users.noreply.github.com>
2021-11-04 11:15:03 -0700

    Set up more readable formating

--- #1a. CONT-CLU1/#3CONT-CLU1.ps1
+++ #1a. CONT-CLU1/#3CONT-CLU1.ps1
@@ -7,7 +7,8 @@ $creddom | Export-Climxl C:\code\creddom.xml
 $VMName = "CONT-CLU1"
 $DomainName = "Thecontoso.com"
 #Send commands to CONT-CLU1
-Invoke-Command -VMName $VMName -Credential $creddom {
+Invoke-Command -VMName $VMName -Credential $creddom
+    {
     #Set up variables that cross over from DC-1 to CONT-CLU1
     #X parameter = VMName , Y parameter = Domain Name
     Param ($x, $y)
@@ -19,13 +20,26 @@ Invoke-Command -VMName $VMName -Credential $creddom {
     Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0
     #Configure firewall for Remote Desktop
     Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
-    # Set static IP address after locating correct interface
-    New-NetIPAddress -InterfaceIndex $(Get-NetAdapter | Where-object {$_.Name -like "*Ethernet*" } | Select-Object -ExpandProperty InterfaceIndex) -IPAddress 192.168.1.160 -PrefixLength 24
+        # Set static IP address after locating correct interface
+    New-NetIPAddress -InterfaceIndex $
+        (Get-NetAdapter | Where-object 
+            {
+            $_.Name -like "*Ethernet*" 
+            }
+            | Select-Object -ExpandProperty InterfaceIndex
+        )
+    -IPAddress 192.168.1.160 -PrefixLength 24
     # Set DNS address to Domain Controller after locating correct interface
-    Set-DnsClientServerAddress -InterfaceIndex $(Get-NetAdapter | Where-object {$_.Name -like "*Ethernet*" } | Select-Object -ExpandProperty InterfaceIndex) -ServerAddresses 192.168.1.152
+    Set-DnsClientServerAddress -InterfaceIndex $
+        (Get-NetAdapter | Where-object 
+            {
+            $_.Name -like "*Ethernet*" 
+            }
+        | Select-Object -ExpandProperty InterfaceIndex)
+    -ServerAddresses 192.168.1.152
     # Join Computer to Domain 
     Add-Computer -DomainName $y -Credential $creddom
     Restart-Computer -Force
-}
+    }
 #Variables that cross over from DC_1 to CONT-CLU1
 -ArgumentList $VMName, $DomainName
