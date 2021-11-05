3055cafb91de199b6eb426eb3a8b5f63a0ce6ac7
christianhjohnson <89424695+christianhjohnson@users.noreply.github.com>
2021-11-05 09:13:32 -0700

    Changed from CLU2 - CLU1

--- #1a. CONT-CLU1/#2 Configure CONT-CLU1.ps1
+++ #1a. CONT-CLU1/#2 Configure CONT-CLU1.ps1
@@ -1,17 +1,17 @@
-<# Configure CONT-CLU2 for Remote Desktop
+<# Configure CONT-CLU1 for Remote Desktop
 Set Static IP and DNS
 Domain Join #>
 #Set up variables for Credentials, VMName, Domain Name, then Export Credentials to XML file for use in future scripts
 $creddom = Get-Credential Administrator
-$VMName = "CONT-CLU2"
-#Send commands to CONT-CLU2
+$VMName = "CONT-CLU1"
+#Send commands to CONT-CLU1
 Invoke-Command -VMName $VMName -Credential $creddom -Scriptblock {
     # Enabling Remote Desktop via Registry
     Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0
     #Configure firewall for Remote Desktop
     Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
     # Set static IP address after locating correct interface
-    New-NetIPAddress -InterfaceIndex $(Get-NetAdapter | Where-object{$_.Name -like "*Ethernet*" } | Select-Object -ExpandProperty InterfaceIndex) -IPAddress 192.168.1.161 -PrefixLength 24 -DefaultGateway 192.168.1.153
+    New-NetIPAddress -InterfaceIndex $(Get-NetAdapter | Where-object{$_.Name -like "*Ethernet*" } | Select-Object -ExpandProperty InterfaceIndex) -IPAddress 192.168.1.160 -PrefixLength 24 -DefaultGateway 192.168.1.153
     # Set Network to Private
     Set-NetConnectionProfile -InterfaceIndex $(Get-NetAdapter | Where-object{$_.Name -like "*Ethernet*" } | Select-Object -ExpandProperty InterfaceIndex) -NetworkCategory Private
     # Set file and print sharing
@@ -19,7 +19,7 @@ Invoke-Command -VMName $VMName -Credential $creddom -Scriptblock {
     # Set DNS Settings
     Set-DnsClientServerAddress -InterfaceIndex $(Get-NetAdapter | Where-object { $_.Name -like "*Ethernet*" } | Select-Object -ExpandProperty InterfaceIndex) -ServerAddresses 192.168.1.153
     #Rename Computer
-    Rename-Computer -NewName "CONT-CLU2"
+    Rename-Computer -NewName "CONT-CLU1"
     # Join Computer to Domain
     $DomCred = Get-Credential thecontoso.com\Administrator
     Add-Computer -DomainName "THECONTOSO.COM" -Credential $DomCred
