3956a2aa684f1727e3cb625be1032972a753c55e
christianhjohnson <89424695+christianhjohnson@users.noreply.github.com>
2021-11-07 12:52:21 -0800

    Added Restart

--- CLU Setup/#4 Install Failover Clustering.ps1
+++ CLU Setup/#4 Install Failover Clustering.ps1
@@ -1,3 +1,2 @@
 $cred = Get-Credential thecontoso.com\administrator
-$CompName = "CONT-CLU1"."CONT-CLU2"
-Invoke-Command -ComputerName $CompName -Credential $cred -scriptblock { Install-WindowsFeature -Name "Failover-Clustering", "Data-Center-Bridging", "RSAT-Clustering-PowerShell", "FS-FileServer", "DHCP" -IncludeManagementTools Remove-WindowsFeature -Name FS-SMB1 -Verbose -Restart }
+Invoke-Command -ComputerName "CONT-CLU1","CONT-CLU2" -Credential $cred -scriptblock { Install-WindowsFeature -Name "Failover-Clustering", "Data-Center-Bridging", "RSAT-Clustering-PowerShell", "FS-FileServer", "DHCP" -IncludeManagementTools -Restart }
