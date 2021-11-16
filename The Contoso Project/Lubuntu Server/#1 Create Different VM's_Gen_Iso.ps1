0dd34d848d6ef8dfcf554ae1c634ac1c900d6702
christianhjohnson <89424695+christianhjohnson@users.noreply.github.com>
2021-11-16 15:47:53 -0800

    Updated #comments on the super script

--- The Contoso Project/Lubuntu Server/#1 Create Different VM's_Gen_Iso.ps1
+++ The Contoso Project/Lubuntu Server/#1 Create Different VM's_Gen_Iso.ps1
@@ -1,11 +1,16 @@
-# Prompt for Name, Number of VMs, and how many data disks are required.
+# Super VM Creation Script
+# GB Convert is used to convert the input from bytes to GB in question for size of drive
 $GBConvert = 1073741824
+# VM Question
 $VMName = Read-Host -Prompt "Enter the name for the VMs"
+# VM Generation sets up a if command to bypass the VM Firmware
 $Generation = Read-Host -Prompt "What Generation of VM (1 or 2)"
 $VMmax = Read-Host -Prompt "Enter the number of VMs to create (1-10000)"
+# Sets up OS for installation, presumes all ISO's are in same directory
 $OS = Read-Host -Prompt "Enter 1 for Server 2016, 2 for Server 2019, 3 for Lubuntu"
-$PrimaryDisk = Read-Host -Prompt "Enter the Drive Letter for the VM Disk (Example C:)"
+$PrimaryDisk = Read-Host -Prompt "Enter the Drive Letter for the VM Disk (Example F:)"
 $Datadisknumber = Read-Host -Prompt "Enter the Number of Datadisks (Up to 4)"
+# Sets up if commands to create the number of data disks, and ask for location of each disk.
 if ($DatadiskNumber -ge 1){
     $1stDataDiskPath = Read-Host -Prompt "Enter the Drive Letter for the 1st Datadisk (Example C:)"
     }
@@ -18,6 +23,7 @@ if ($DatadiskNumber -ge 3){
 if ($DatadiskNumber -ge 4){
         $4thDataDiskPath = Read-Host -Prompt "Enter the Drive Letter for the 4st Datadisk (Example C:)"
     }
+# If Datadisk selection =0 then it bypasses the enter size of datadisk question
 if ($Datadisknumber -ne 0){
     [Int]$Datadisksize = Read-Host -Prompt "Enter the size of Datadisks in GB"
     [Int64]$DatadisksizeGB = $Datadisksize * $GBConvert
@@ -29,7 +35,7 @@ for ($VMNumber = 1; $VMNumber -le $VMmax; $VMNumber++)
     {
         #Create VM's
         New-VM -Name $VMNAME$VMNumber -Generation $Generation -NewVHDPath $PrimaryDisk\VMs\$VMNAME$VMNumber.vhdx -NewVHDSizeBytes 80GB -Switch $Switch
-        # Configure VM Memory
+        # Configure VM operating systems according to previous question
         if ($OS -eq 1){
             Add-VMDVDDrive -VMName $VMNAME$VMNumber -Path C:\ISOs\WindowsServer2016.iso
             }
@@ -40,15 +46,15 @@ for ($VMNumber = 1; $VMNumber -le $VMmax; $VMNumber++)
             Add-VMDVDDrive -VMName $VMNAME$VMNumber -Path C:\ISOs\Lubuntu.iso
             }
         Set-VMProcessor $VMNAME$VMNumber -Count $NumberofCores -ExposeVirtualizationExtensions $true
-        #Setup Variables for DVD and Primary Drive for Boot Order
+        #Setup Variables for DVD and Primary Drive for Boot Order only if Gen 2
         if ($Generation -eq 2){
             $DVDDrive = Get-VMDvdDrive $VMNAME$VMNumber
             $primaryDrive = Get-VMHardDiskDrive -VMName $VMNAME$VMNumber -ControllerLocation 0
             Set-VMFirmware $VMNAME$VMNumber -BootOrder $primaryDrive, $DVDDrive
             }
-        #Set-VM $VMNAME$VMNumber -DynamicMemory -MemoryMinimumBytes $MinMemorySizeMB -MemoryMaximumBytes $MaxMemoryMB -MemoryStartupBytes -$StartupMemoryMB 
+        # no longer needed Set-VM $VMNAME$VMNumber -DynamicMemory -MemoryMinimumBytes $MinMemorySizeMB -MemoryMaximumBytes $MaxMemoryMB -MemoryStartupBytes -$StartupMemoryMB 
         Set-VMMemory $VMNAME$VMNumber -DynamicMemoryEnabled $true
-    #Create Datadisks
+    #Create Datadisks according to the number needed.
     if ($Datadisknumber -ge 1)
             {
             New-VHD -Path "$1stDataDiskPath\VMDataDisks\$VMNAME$VMNumber\Datadisk1.vhdx" -Dynamic -SizeBytes $DatadisksizeGB
