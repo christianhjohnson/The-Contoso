4853688e2b81459ae00b263fa57c959dc0ac7bad
christianhjohnson <89424695+christianhjohnson@users.noreply.github.com>
2021-12-30 15:23:34 -0800

    Create Parameter JSON with PowerShell deployment

---
+++ ARM Templates/Deploy Parameter-JSON.ps1
@@ -0,0 +1,9 @@
+$rg + 'armtest-01'
+New-AzResourceGroup -Name $rg -Location westus -Force
+
+New-AzResourceGroupDeployment `
+-Name 'new-storage' `
+-ResourceGroupName $rg `
+-TemplateParameterFile 'JSON-storage' `
+-storageName 'vastmegastorge'
+-storageSKU 'Standard_LRS'
